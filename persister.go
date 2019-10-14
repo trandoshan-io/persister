1d752a0b92fa6e66f893014df5c9c952e2235afd
Aloïs Micard <lunamicard@gmail.com>
2019-10-14 22:34:24 +0200

    Now persist content on disk
    - [#7] Remove MongoDB usage: now persist content directly on disk
    - [#3] Do not delete existing content: now store content no matter what

--- persister.go
+++ persister.go
@@ -1,16 +1,13 @@
 package main
 
 import (
-	"context"
 	"encoding/json"
 	"fmt"
 	"github.com/nats-io/nats.go"
-	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
-	"go.mongodb.org/mongo-driver/mongo/readpref"
+	"io/ioutil"
 	"log"
 	"os"
+	"regexp"
 	"strings"
 	"time"
 )
@@ -18,11 +15,13 @@ import (
 const (
 	contentQueue   = "contentQueue"
 	contentSubject = "contentSubject"
-
-	trandoshanDatabase = "trandoshan"
-	resourceCollection = "resources"
 )
 
+var (
+	protocolRegex = regexp.MustCompile("https?://")
+)
+
+// Json data received from NATS
 type resourceData struct {
 	Url     string `json:"url"`
 	Content string `json:"content"`
@@ -30,16 +29,6 @@ type resourceData struct {
 
 func main() {
 	log.Print("Initializing persister")
-
-	// initialize and validate database connection
-	ctx, _ := context.WithTimeout(context.Background(), 2*time.Second)
-	client, err := mongo.Connect(ctx, options.Client().ApplyURI(os.Getenv("MONGO_URI")))
-	if err != nil {
-		log.Fatalf("Unable to create database connection: %s", err)
-	}
-	if err := client.Ping(ctx, readpref.Primary()); err != nil {
-		log.Fatalf("Unable to connect to database: %s", err)
-	}
 
 	// connect to NATS server
 	nc, err := nats.Connect(os.Getenv("NATS_URI"))
@@ -49,7 +38,7 @@ func main() {
 	defer nc.Close()
 
 	// initialize queue subscriber
-	if _, err := nc.QueueSubscribe(contentSubject, contentQueue, handleMessages(client)); err != nil {
+	if _, err := nc.QueueSubscribe(contentSubject, contentQueue, handleMessages()); err != nil {
 		log.Fatalf("Error while trying to subscribe to server: %s", err)
 	}
 
@@ -59,12 +48,8 @@ func main() {
 	select {}
 }
 
-func handleMessages(client *mongo.Client) func(*nats.Msg) {
-	resourceCollection := client.Database(trandoshanDatabase).Collection(resourceCollection)
+func handleMessages() func(*nats.Msg) {
 	return func(msg *nats.Msg) {
-		// setup production context
-		ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
-
 		var data resourceData
 
 		// Unmarshal message
@@ -74,31 +59,32 @@ func handleMessages(client *mongo.Client) func(*nats.Msg) {
 			return
 		}
 
-		// Determinate if entry does not already exist
-		resource, err := getResource(client, data.Url)
-		if err != nil {
-			log.Printf("Error while checking if url exist. Considering not. %s", err)
-		}
-		// there is a previous result
-		if resource != nil {
-			log.Printf("Url: %s is already crawled. Deleting old entry", data.Url)
+		// Store content in the filesystem
+		currentTime := time.Now()
+		filePath := fmt.Sprintf("%s/%s", os.Getenv("STORAGE_PATH"), computePath(data.Url, currentTime))
+		log.Printf("Storing content on path: %s", filePath)
 
-			// delete old entry
-			if _, err := resourceCollection.DeleteOne(ctx, bson.M{"url": data.Url}); err != nil {
-				log.Printf("Error while deleting old entry. Cancelling persist request. %s", err)
-				// todo: store in sort of DLQ?
-				return
-			}
-		}
-
-		// Finally create entry in database
-		_, err = resourceCollection.InsertOne(ctx, bson.M{"url": data.Url, "crawlDate": time.Now(), "title": extractTitle(data.Content), "content": data.Content})
-		if err != nil {
-			log.Printf("Error while saving content: %s", err)
-			// todo: store in sort of DLQ?
+		if err := ioutil.WriteFile(filePath, []byte(data.Content), 0644); err != nil {
+			log.Printf("Error while trying to save content: %s", err)
 			return
 		}
+
+		// todo call Elasticsearch and create model from content
 	}
+}
+
+// Compute path for resource storage using his URL and the crawling time
+// Format is: resource-url/64bit-timestamp
+// f.e: http://login.google.com/secure/createAccount.html -> login.google.com/secure/createAccount.html/1570788418
+func computePath(resourceUrl string, crawlData time.Time) string {
+	// first of all sanitize resource URL
+	var sanitizedResourceUrl string
+	// remove protocol
+	sanitizedResourceUrl = protocolRegex.ReplaceAllLiteralString(resourceUrl, "")
+	// remove any trailing '/'
+	sanitizedResourceUrl = strings.TrimSuffix(sanitizedResourceUrl, "/")
+
+	return fmt.Sprintf("%s/%d", sanitizedResourceUrl, crawlData.Unix())
 }
 
 // Extract title from given html
@@ -112,20 +98,4 @@ func extractTitle(body string) string {
 		return ""
 	}
 	return body[startPos:endPos]
-}
-
-// Get resource using his url
-// todo: call API instead?
-func getResource(client *mongo.Client, url string) (*resourceData, error) {
-	// Setup production context and acquire database collection
-	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
-	resourceCollection := client.Database(trandoshanDatabase).Collection(resourceCollection)
-
-	// Query database for result
-	var resource resourceData
-	if err := resourceCollection.FindOne(ctx, bson.M{"url": url}).Decode(&resource); err != nil {
-		return nil, fmt.Errorf("error while decoding result: %s", err)
-	}
-
-	return &resource, nil
 }
--- persister_test.go
+++ persister_test.go
@@ -1,11 +1,15 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+	"time"
+)
 
 func TestExtractTitlePresentLowerCase(t *testing.T) {
 	body := "this is a dummy<title>test page</title> to make sure title extracting works great"
-
 	title := extractTitle(body)
+
 	if title != "test page" {
 		t.Errorf("Found title: %v is not 'test page'", title)
 	}
@@ -13,8 +17,8 @@ func TestExtractTitlePresentLowerCase(t *testing.T) {
 
 func TestExtractTitlePresentUpperCaseAndPreserveCase(t *testing.T) {
 	body := "this is A dummy<TITLE>test PaGe</TITLE> to MAKE sure title EXTRACTING works great"
-
 	title := extractTitle(body)
+
 	if title != "test PaGe" {
 		t.Errorf("Found title: %v is not 'test PaGe'", title)
 	}
@@ -22,9 +26,27 @@ func TestExtractTitlePresentUpperCaseAndPreserveCase(t *testing.T) {
 
 func TestExtractTitleNotPresent(t *testing.T) {
 	body := "this is a dummy to make sure title extracting works great"
-
 	title := extractTitle(body)
+
 	if title != "" {
 		t.Errorf("Found title but no one are present")
+	}
+}
+
+func TestComputePathNoExtension(t *testing.T) {
+	currentTime := time.Now()
+	path := computePath("https://private.creekorful.fr/something/strange/", currentTime)
+
+	if path != fmt.Sprintf("private.creekorful.fr/something/strange/%d", currentTime.Unix()) {
+		t.Errorf("Computed path is wrong")
+	}
+}
+
+func TestComputePathWithExtension(t *testing.T) {
+	currentTime := time.Now()
+	path := computePath("http://private.creekorful.fr/something/strange/index.php", currentTime)
+
+	if path != fmt.Sprintf("private.creekorful.fr/something/strange/index.php/%d", currentTime.Unix()) {
+		t.Errorf("Computed path is wrong")
 	}
 }
